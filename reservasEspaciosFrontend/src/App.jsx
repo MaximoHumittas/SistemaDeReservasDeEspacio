31d6c245505d769bd0760c46ad383c2d5158562c
Maximo Mora <maxmora401@gmail.com>
2024-09-03 14:52:29 -0400

    prueba de imagen de gato

--- reservasEspaciosFrontend/src/App.jsx
+++ reservasEspaciosFrontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import cat from './assets/images/cat.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <div>
+        <img src={cat} alt="" />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Branch de max</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ reservasEspaciosFrontend/src/components/PhotoCard.jsx
@@ -0,0 +1,16 @@
+
+
+export function PhotoCard({photo,name}) {
+
+
+    return (
+        <article>
+
+            <h1>{name}</h1>
+            
+        </article>
+
+
+    )
+    
+}
