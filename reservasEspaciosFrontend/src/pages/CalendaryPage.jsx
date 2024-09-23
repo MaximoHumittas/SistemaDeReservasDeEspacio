eba46ccac830d419d67431597f583d5189bb06e6
Maximo Mora <maxmora401@gmail.com>
2024-09-23 17:13:39 -0300

    basico formulario y calendario

--- reservasEspaciosFrontend/src/pages/CalendaryPage.jsx
+++ reservasEspaciosFrontend/src/pages/CalendaryPage.jsx
@@ -1,9 +1,34 @@
+import { Calendar, dayjsLocalizer } from 'react-big-calendar'; 
+import 'react-big-calendar/lib/css/react-big-calendar.css'; 
+import dayjs from 'dayjs';
 
-export default function Calendary(){
+const localizer = dayjsLocalizer(dayjs);
 
-    return (
-        <div>
-            <h1>Calendary</h1>
-        </div>
-    )
+const events = [
+  {
+    title: 'Cubiculo, cubiculo 1',
+    start: new Date(),
+    end: new Date(),
+  },
+  {
+    title: 'Evento 2',
+    start: new Date(2024, 9, 24, 12, 0), 
+    end: new Date(2024, 9, 24, 14, 0),
+  },
+];
+
+export default function Calendary() {
+  return (
+    <div>
+      <h1>Pruebas crear agendamientos</h1>
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
+    </div>
+  );
 }
--- reservasEspaciosFrontend/src/pages/FormularyPage.jsx
+++ reservasEspaciosFrontend/src/pages/FormularyPage.jsx
@@ -2,8 +2,99 @@
 export default function Formulary(){
 
     return (
+        
+
         <div>
-            <h1>Formulary</h1>
-        </div>
+
+        <h1>Pruebas crear agendamientos </h1>
+
+
+        <h2>Registro de reserva </h2>
+
+        <form >
+
+            <div>
+                <label>tipo de agendamiento</label>
+                <select
+
+                    name="tipo_agendamiento"
+                    id="tipo_agendamiento"
+                 
+                    
+
+
+                >
+                    <option value="">Seleccionar ...</option>
+                    <option value="CUBICULO">Cubículo</option>
+                    <option value="GIMNASIO">Gimnasio</option>
+                </select>
+            </div>
+
+
+            <div>
+                <label >Seleccionar Dia</label>
+
+            </div>
+
+            <div>
+                <label>Seleccionar Horario</label>
+                <select 
+                    name="horario" 
+                    id="horario" 
+          
+
+                >
+                    <option value="">Seleccionar ...</option>
+
+                </select>
+            </div>
+
+
+            <button type='submit'>Agendar Fecha</button>
+        </form>    
+
+
+
+        <h2>Registrar de los recursos reservables</h2>  
+
+        <form >
+
+            <div>
+                <label>Tipo de recurso</label>
+                <select 
+                    name="tipo_recurso" 
+                    id="tipo_recurso"
+                   
+                >
+                    <option value="">Seleccionar...</option>
+                    <option value="CUBICULO">Cubículo</option>
+                    <option value="GIMNASIO">Gimnasio</option>
+                </select>
+
+            </div>
+
+            <div>
+                <label htmlFor="text">Nombre del recurso</label>
+                <input type="text" name='nombre'  />
+            </div>
+
+
+            <button type='submit' >Registrar Recurso</button>
+
+            
+
+        </form>
+
+
+
+
+
+
+
+
+
+
+    </div>
+
     )
 }
