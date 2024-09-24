f2509325b723a0c148e2ca8851516344c9efedec
Maximo Mora <maxmora401@gmail.com>
2024-09-24 04:26:57 -0300

    reestructuracion de mi rama

--- reservasEspaciosBackend/db.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const { createClient } = require('@supabase/supabase-js');
-
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
-
-module.exports = supabase;
--- reservasEspaciosFrontend/src/hooks/useAuth.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import { UserContext } from '../userContext';
-
-export function useAuth() {
-  const { user } = useContext(UserContext);
-  return {
-    isAuthenticated: !!user, 
-  };
-}
