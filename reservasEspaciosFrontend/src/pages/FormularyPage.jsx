d34aa2b2058e1a89366fa6ce9c3c53ddfd375b93
MATIASVLL18 <mvalenzuela2022@alu.uct.cl>
2024-09-30 00:43:20 -0300

    formulario validacion de inputs  con restriccion
    prohíbe el acceso de los demás inputs del formulario sin antes haber seleccionado el tipo de recurso del formulario como prioridad  , aparte cree un div encima de los inputs desahabilitados  para que cuando el usuario haga click a pesar de que no te de la opcion salga un mensaje de alerta

--- reservasEspaciosFrontend/src/pages/FormularyPage.jsx
+++ reservasEspaciosFrontend/src/pages/FormularyPage.jsx
@@ -1,4 +1,3 @@
-
 import 'react-big-calendar/lib/css/react-big-calendar.css'; 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -6,89 +5,83 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import React from 'react';
 import { reserveRequest, resourceRequest, hoursRequest } from '../api/auth';
-
 import { format } from 'date-fns';
 
 export default function Formulary(){
 
-    const { register, handleSubmit, setValue,getValues  } = useForm();
+    const { register, handleSubmit, setValue, getValues  } = useForm();
     const [startDate, setStartDate] = useState(null);
     const [availableHours, setAvailableHours] = useState([]);
-    const [busyHours,setBusyHours] = useState([])
-    const [idResources,setIdResources] = useState([])
-
+    const [busyHours, setBusyHours] = useState([]);
+    const [idResources, setIdResources] = useState([]);
+    const [isResourceTypeSelected, setIsResourceTypeSelected] = useState(false);//indica si el usuario ha seleccionado un tipo de recurso.
+    const [errorMessages, setErrorMessages] = useState({
+        date: '',
+        hour: '',
+    });
 
     const GetResourcesByType = async (event) => {
-
-        const resourceType = event.target.value
-
+        const resourceType = event.target.value;
         if (resourceType) {
             try {
-                const response = await resourceRequest(event.target.value)
-        
-                setIdResources(response)
-                console.log("datos del register: ",register)
+                const response = await resourceRequest(resourceType);
+                setIdResources(response);
+                setIsResourceTypeSelected(true);
+                setErrorMessages({ date: '', hour: '' }); // Limpiar mensajes de error
             } catch (error) {
-                console.log("Error en obtner el dato",error)
+                console.log("Error en obtener el dato", error);
             }
-        }    
-    }
+        } else {
+            setIsResourceTypeSelected(false);
+            setErrorMessages({ date: '', hour: '' });
+        }
+    };
+
+    //se ejecuta cuando el usuario intenta interactuar con un input deshabilitado
+    const handleBlockedClick = (field) => {
+        if (!isResourceTypeSelected) {
+            setErrorMessages(prevState => ({
+                ...prevState,
+                [field]: "Por favor, selecciona primero el tipo de recurso."
+            }));
+        }
+    };
 
     const GetHoraryById = async (dateDataPicker) => {
         const dataFormated = format(dateDataPicker, 'yyyy-MM-dd');
         setValue("date", dataFormated);
-        
-        
-        console.log("ID Resources:", idResources);
-        console.log("ID Resources:", idResources['data'][0]);
-        setValue("resourceId",idResources['data'][0]['id'])
-        console.log("Formatted date:", dataFormated);
-        let auxBusyHours = []
-        let auxAvailableHours = []
-        
+        setValue("resourceId", idResources['data'][0]['id']);
+
+        let auxBusyHours = [];
+        let auxAvailableHours = [];
+
         try {
             const response = await hoursRequest(idResources['data'][0], dataFormated);
-    
-            if (response) {  
-                console.log("response data", response);
-                console.log(response.length)
-
+            if (response) {
                 for (let index = 0; index < response.length; index++) {
-                    
-                    const starTime = response[index]['hora_inicio'].substring(0,5)
-                    const endTime = response[index]['hora_fin'].substring(0,5)
-
-               
+                    const starTime = response[index]['hora_inicio'].substring(0, 5);
+                    const endTime = response[index]['hora_fin'].substring(0, 5);
                     auxBusyHours.push(starTime + ' - ' + endTime);
                 }
-                setBusyHours(auxBusyHours)
+                setBusyHours(auxBusyHours);
 
-                for (let index = 8; index <= 19; index ++) {
-
-                    let startTime = `${index}:00`
-                    let endTime = `${index+1}:00`
+                for (let index = 8; index <= 19; index++) {
+                    let startTime = `${index}:00`;
+                    let endTime = `${index + 1}:00`;
 
                     if (index <= 9) {
-                        startTime = `0${index}:00`
-
-                        if (index == 9) {
-                            endTime = `${index+1}:00`
-                            
+                        startTime = `0${index}:00`;
+                        if (index === 9) {
+                            endTime = `${index + 1}:00`;
                         } else {
-
-                            endTime = `0${index+1}:00`
-                        }              
+                            endTime = `0${index + 1}:00`;
+                        }
                     }
-                    auxAvailableHours.push(startTime + ' - ' +endTime)
+                    auxAvailableHours.push(startTime + ' - ' + endTime);
                 }
 
-                console.log("Aux available hours ",auxAvailableHours)
-                console.log("Aux busy hours ",auxBusyHours)
                 const availableHours = auxAvailableHours.filter(hour => !auxBusyHours.includes(hour));
-
-                setAvailableHours(availableHours)
-    
-        
+                setAvailableHours(availableHours);
             } else {
                 console.log("No data found in the response");
             }
@@ -98,18 +91,15 @@ export default function Formulary(){
     };
 
     const onSubmit = async (values) => {
-        console.log("Valores que se se envian a reserva ",values);
         try {
             const response = await reserveRequest(values);
-            console.log(response)
+            console.log(response);
         } catch (error) {
             console.log("Error al enviar la solicitud:", error);
         }
     };
 
-
     return (
-
         <div>
             <h1>Formulario</h1>
             <form onSubmit={handleSubmit(onSubmit)}>
@@ -123,31 +113,70 @@ export default function Formulary(){
                         <option value="CUBICULO">Cubiculo</option>
                         <option value="GIMNASIO">Gimnasio</option>
                     </select>
-                    <div>
-                        <label>Seleccionar Dia</label>
-                        <DatePicker
-                            selected={startDate}
-                            onChange={(date) => {
-                                setStartDate(date);
-                                GetHoraryById(date);
-                            }} 
-                            dateFormat="yyyy/MM/dd" 
-                            placeholderText="Seleccionar fecha"
-                            required 
-                        />
-                    </div>
                 </div>
-                <label>Elige la hora</label>
-                <select {...register("hour", { required: "Selecciona una hora" })}>
-                    <option value="">Seleccionar...</option>
-                    {availableHours.map(hour => (
-                        <option key={hour} value={hour}>{hour}</option>
-                    ))}
-                </select>
-                <button type="submit">Enviar</button>
+                <div style={{ position: 'relative' }}>
+                    <label>Seleccionar Día</label>
+                    <DatePicker
+                        selected={startDate}
+                        onChange={(date) => {
+                            setStartDate(date);
+                            GetHoraryById(date);
+                        }} 
+                        dateFormat="yyyy/MM/dd" 
+                        placeholderText="Seleccionar fecha"
+                        required 
+                        disabled={!isResourceTypeSelected} 
+                    />
+                    {!isResourceTypeSelected && (
+                        //div captura los intentos de clic en los inputs cuando están deshabilitados y muestra un mensaje de error al usuario.
+                        <div 
+                            style={{
+                                position: 'absolute',
+                                top: 0,
+                                left: 0,
+                                width: '100%',
+                                height: '100%',
+                                background: 'transparent',
+                                cursor: 'not-allowed'
+                            }}
+                            onClick={() => handleBlockedClick('date')}
+                        />
+                    )}
+                    {errorMessages.date && (
+                        <p style={{ color: 'red' }}>{errorMessages.date}</p>
+                    )}
+                </div>
+                <div style={{ position: 'relative' }}>
+                    <label>Seleccione la hora</label>
+                    <select 
+                        {...register("hour", { required: "Selecciona una hora" })}
+                        disabled={!isResourceTypeSelected} 
+                    >
+                        <option value="">Seleccionar...</option>
+                        {availableHours.map(hour => (
+                            <option key={hour} value={hour}>{hour}</option>
+                        ))}
+                    </select>
+                    {!isResourceTypeSelected && (
+                        <div 
+                            style={{
+                                position: 'absolute',
+                                top: 0,
+                                left: 0,
+                                width: '100%',
+                                height: '100%',
+                                background: 'transparent',
+                                cursor: 'not-allowed'
+                            }}
+                            onClick={() => handleBlockedClick('hour')}
+                        />
+                    )}
+                    {errorMessages.hour && (
+                        <p style={{ color: 'red' }}>{errorMessages.hour}</p>
+                    )}
+                </div>
+                <button type="submit" disabled={!isResourceTypeSelected}>Enviar</button>
             </form>
         </div>
-
-        
-    )
+    );
 }
