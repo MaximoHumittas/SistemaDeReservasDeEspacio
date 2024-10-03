99fb53128fc435ff8dadf692fcd6bc8a4b7e2d93
Diego-Anthony18 <diegoanthony.ac1812@gmail.com>
2024-10-03 16:03:02 -0300

    Calendario filtrado
    Se incorporo botones de filtrado en la visualizacion del calendario, reducido a la mera funcionalidad, sin contexto de la BBDD

--- reservasEspaciosFrontend/src/components/Calendary.jsx
+++ reservasEspaciosFrontend/src/components/Calendary.jsx
@@ -1,33 +1,76 @@
+import { useState } from 'react';
 import { Calendar, dayjsLocalizer } from 'react-big-calendar'; 
 import 'react-big-calendar/lib/css/react-big-calendar.css'; 
 import dayjs from 'dayjs';
+import '../styles/calendar.css';  
 
 const localizer = dayjsLocalizer(dayjs);
 
-const events = [
+const allEvents = [
   {
     title: 'Cubiculo, cubiculo 1',
     start: new Date(),
     end: new Date(),
+    type: 'CUBICULO',
+    style: { backgroundColor: 'blue', color: 'white' }, 
   },
   {
-    title: 'Gimnasio 4',
-    start: new Date(2024, 9, 27, 12, 0), 
-    end: new Date(2024, 9, 24, 14, 0),
+    title: 'Biblioteca, sala 2',
+    start: new Date(2024, 9, 24, 12, 0), 
+    end: new Date(2024, 9, 24, 14, 0),  
+    type: 'BIBLIOTECA',
+    style: { backgroundColor: 'red', color: 'white' }, 
   },
 ];
 
 export default function Calendary() {
+  const [selectedFilters, setSelectedFilters] = useState({
+    CUBICULO: true,
+    BIBLIOTECA: true,
+  });
+
+  const filteredEvents = allEvents.filter(event => selectedFilters[event.type]);
+
+  const handleFilterToggle = (type) => {
+    setSelectedFilters(prevState => ({
+      ...prevState,
+      [type]: !prevState[type],
+    }));
+  };
+
   return (
     <div>
       <h1>Pruebas crear agendamientos</h1>
-      <Calendar
-        localizer={localizer}
-        events={events}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500 }}
-      />
+      
+      {/*BOTONES DE FILTRADO */}
+      <div>
+        <button
+          className={`calendar-button button-cubiculo ${selectedFilters.CUBICULO ? 'button-active' : 'button-inactive'}`}
+          onClick={() => handleFilterToggle('CUBICULO')}
+        >
+          CUBICULO
+        </button>
+        <button
+          className={`calendar-button button-biblioteca ${selectedFilters.BIBLIOTECA ? 'button-active' : 'button-inactive'}`}
+          onClick={() => handleFilterToggle('BIBLIOTECA')}
+        >
+          BIBLIOTECA
+        </button>
+      </div>
+
+      {/* CALENDARIO COMO TAL */}
+      <div className="calendar-container">
+        <Calendar
+          localizer={localizer}
+          events={filteredEvents}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 500 }}
+          eventPropGetter={(event) => ({
+            style: event.style || {},
+          })}
+        />
+      </div>
     </div>
   );
 }
