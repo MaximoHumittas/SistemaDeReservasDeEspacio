6b7957a26cd88e48d246c9ea2abe778905cc0848
MATIASVLL18 <mvalenzuela2022@alu.uct.cl>
2024-10-21 19:25:09 -0300

    roles usuario +reserva
    reserva los tipos de recurso segun tu rol , insercion de reservas y eliminar tu reserva

--- reservasEspaciosFrontend/src/components/Calendary.jsx
+++ reservasEspaciosFrontend/src/components/Calendary.jsx
@@ -1,36 +1,23 @@
 import { useState } from 'react';
-import { Calendar, dayjsLocalizer } from 'react-big-calendar'; 
-import 'react-big-calendar/lib/css/react-big-calendar.css'; 
+import { Calendar, dayjsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 import dayjs from 'dayjs';
 import '../styles/Calendar.css';  
 
 const localizer = dayjsLocalizer(dayjs);
 
-const allEvents = [
-  {
-    title: 'Cubiculo, cubiculo 1',
-    start: new Date(),
-    end: new Date(),
-    type: 'CUBICULO',
-    style: { backgroundColor: 'blue', color: 'white' }, 
-  },
-  {
-    title: 'Biblioteca, sala 2',
-    start: new Date(2024, 9, 24, 12, 0), 
-    end: new Date(2024, 9, 24, 14, 0),  
-    type: 'BIBLIOTECA',
-    style: { backgroundColor: 'red', color: 'white' }, 
-  },
-];
-
-export default function Calendary() {
+export default function Calendary({ events, onDeleteEvent }) {
   const [selectedFilters, setSelectedFilters] = useState({
     CUBICULO: true,
     BIBLIOTECA: true,
+    GIMNASIO: true,
+    SALA: true,
   });
 
-  const filteredEvents = allEvents.filter(event => selectedFilters[event.type]);
+  // Filtrar eventos según el filtro seleccionado
+  const filteredEvents = events.filter(event => selectedFilters[event.type]);
 
+  // Función para alternar los filtros
   const handleFilterToggle = (type) => {
     setSelectedFilters(prevState => ({
       ...prevState,
@@ -38,27 +25,75 @@ export default function Calendary() {
     }));
   };
 
+  // Función para asignar color a cada tipo de evento
+  const eventStyleGetter = (event) => {
+    let backgroundColor = '';
+    
+    switch (event.type) {
+      case 'BIBLIOTECA':
+        backgroundColor = 'red';
+        break;
+      case 'CUBICULO':
+        backgroundColor = 'blue';
+        break;
+      case 'GIMNASIO':
+        backgroundColor = 'green';
+        break;
+      case 'SALA':
+        backgroundColor = 'yellow';
+        break;
+      default:
+        backgroundColor = 'gray';
+    }
+
+    return {
+      style: {
+        backgroundColor,
+        color: 'white',
+        borderRadius: '5px',
+        border: 'none',
+        padding: '5px',
+      },
+    };
+  };
+
   return (
     <div>
       <h1>Pruebas crear agendamientos</h1>
-      
-      {/*BOTONES DE FILTRADO */}
+
+      {/* BOTONES DE FILTRADO */}
       <div>
         <button
           className={`calendar-button button-cubiculo ${selectedFilters.CUBICULO ? 'button-active' : 'button-inactive'}`}
           onClick={() => handleFilterToggle('CUBICULO')}
+          style={{ backgroundColor: 'blue', color: 'white' }}
         >
           CUBICULO
         </button>
         <button
           className={`calendar-button button-biblioteca ${selectedFilters.BIBLIOTECA ? 'button-active' : 'button-inactive'}`}
           onClick={() => handleFilterToggle('BIBLIOTECA')}
+          style={{ backgroundColor: 'red', color: 'white' }}
         >
           BIBLIOTECA
         </button>
+        <button
+          className={`calendar-button button-gimnasio ${selectedFilters.GIMNASIO ? 'button-active' : 'button-inactive'}`}
+          onClick={() => handleFilterToggle('GIMNASIO')}
+          style={{ backgroundColor: 'green', color: 'white' }}
+        >
+          GIMNASIO
+        </button>
+        <button
+          className={`calendar-button button-sala ${selectedFilters.SALA ? 'button-active' : 'button-inactive'}`}
+          onClick={() => handleFilterToggle('SALA')}
+          style={{ backgroundColor: 'yellow', color: 'black' }}
+        >
+          SALA
+        </button>
       </div>
 
-      {/* CALENDARIO COMO TAL */}
+      {/* CALENDARIO */}
       <div className="calendar-container">
         <Calendar
           localizer={localizer}
@@ -66,9 +101,8 @@ export default function Calendary() {
           startAccessor="start"
           endAccessor="end"
           style={{ height: 500 }}
-          eventPropGetter={(event) => ({
-            style: event.style || {},
-          })}
+          eventPropGetter={eventStyleGetter}  // Asigna los colores a los eventos
+          onSelectEvent={onDeleteEvent}  // Pasamos la función para eliminar el evento
         />
       </div>
     </div>
--- reservasEspaciosFrontend/src/pages/FormularyPage.jsx
+++ reservasEspaciosFrontend/src/pages/FormularyPage.jsx
@@ -1,41 +1,53 @@
+import axios from 'axios';
+
 import 'react-big-calendar/lib/css/react-big-calendar.css'; 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useState,useContext } from 'react';
+import { useState, useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { reserveRequest, resourceRequest, hoursRequest } from '../api/auth';
 import { format } from 'date-fns';
-import Calendary from '../components/Calendary'; // Importa el nuevo componente
+import Calendary from '../components/Calendary'; // Importa el componente Calendary
 import { AuthContext } from "../context/AuthContext";
+import '../styles/Formulary.css'; // Importa la hoja de estilos CSS
 
 export default function Formulary() {
-
   const { user } = useContext(AuthContext);
 
-  const { register, handleSubmit, setValue, getValues } = useForm();
+  const { register, handleSubmit, setValue } = useForm();
   const [startDate, setStartDate] = useState(null);
   const [availableHours, setAvailableHours] = useState([]);
-  const [busyHours, setBusyHours] = useState([]);
   const [idResources, setIdResources] = useState([]);
-  const [isResourceTypeSelected, setIsResourceTypeSelected] = useState(false); // Indicador si se seleccionó tipo de recurso
-  //id 
-
+  const [isResourceTypeSelected, setIsResourceTypeSelected] = useState(false);
+  const [selectedResourceType, setSelectedResourceType] = useState('');
+  const [events, setEvents] = useState([]); // Estado para los eventos del calendario
   const [errorMessages, setErrorMessages] = useState({
     date: '',
     hour: '',
   });
 
-  setValue("idUser", user.id )
+  // Verificamos si user existe y tiene un rol asignado
+  if (!user) {
+    return <p>Cargando información del usuario...</p>;
+  }
+
+  // Obtener recursos según el tipo seleccionado
   const GetResourcesByType = async (event) => {
     const resourceType = event.target.value;
+    setSelectedResourceType(resourceType);
     if (resourceType) {
       try {
         const response = await resourceRequest(resourceType);
-        setIdResources(response);
-        setIsResourceTypeSelected(true);
-        setErrorMessages({ date: '', hour: '' }); // Limpiar mensajes de error
+        if (response.length > 0) {
+          setIdResources(response);
+          setValue("recurso_id", response[0].id); 
+          setIsResourceTypeSelected(true);
+          setErrorMessages({ date: '', hour: '' });
+        } else {
+          setErrorMessages({ date: '', hour: 'No hay recursos disponibles para este tipo.' });
+        }
       } catch (error) {
-        console.log("Error al obtener el dato", error);
+        console.log("Error al obtener los recursos", error);
       }
     } else {
       setIsResourceTypeSelected(false);
@@ -43,94 +55,134 @@ export default function Formulary() {
     }
   };
 
-  const handleBlockedClick = (field) => {
-    if (!isResourceTypeSelected) {
-      setErrorMessages(prevState => ({
-        ...prevState,
-        [field]: "Por favor, selecciona primero el tipo de recurso."
-      }));
+  const GetHoraryById = async (dateDataPicker) => {
+    const dataFormated = format(dateDataPicker, 'yyyy-MM-dd');
+    setValue("fecha", dataFormated);
+
+    if (idResources.length > 0) {
+      const recurso_id = idResources[0].id; 
+      setValue("recurso_id", recurso_id);
+
+      let auxBusyHours = [];
+      let auxAvailableHours = [];
+
+      try {
+        const response = await hoursRequest(recurso_id, dataFormated);
+        if (response) {
+          for (let index = 0; index < response.length; index++) {
+            const starTime = response[index]['hora_inicio'].substring(0, 5);
+            const endTime = response[index]['hora_fin'].substring(0, 5);
+            auxBusyHours.push(starTime + ' - ' + endTime);
+          }
+          setAvailableHours(generateAvailableHours(auxBusyHours));
+        }
+      } catch (error) {
+        console.log("Error al obtener los horarios", error);
+      }
     }
   };
 
-  const GetHoraryById = async (dateDataPicker) => {
-    const dataFormated = format(dateDataPicker, 'yyyy-MM-dd');
-    setValue("date", dataFormated);
-    console.log(idResources)
-    setValue("resourceId", idResources[0]['id']);
-
-    let auxBusyHours = [];
+  const generateAvailableHours = (busyHours) => {
     let auxAvailableHours = [];
+    for (let index = 8; index <= 19; index++) {
+      let startTime = `${index}:00`;
+      let endTime = `${index + 1}:00`;
 
-    console.log('id Resources', idResources[0])
-    console.log('data formated ', dataFormated)
-
-    try {
-      const response = await hoursRequest(idResources[0], dataFormated);
-      if (response) {
-        for (let index = 0; index < response.length; index++) {
-          const starTime = response[index]['hora_inicio'].substring(0, 5);
-          const endTime = response[index]['hora_fin'].substring(0, 5);
-          auxBusyHours.push(starTime + ' - ' + endTime);
+      if (index <= 9) {
+        startTime = `0${index}:00`;
+        if (index === 9) {
+          endTime = `${index + 1}:00`;
+        } else {
+          endTime = `0${index + 1}:00`;
         }
-        setBusyHours(auxBusyHours);
-
-        for (let index = 8; index <= 19; index++) {
-          let startTime = `${index}:00`;
-          let endTime = `${index + 1}:00`;
-
-          if (index <= 9) {
-            startTime = `0${index}:00`;
-            if (index === 9) {
-              endTime = `${index + 1}:00`;
-            } else {
-              endTime = `0${index + 1}:00`;
-            }
-          }
-          auxAvailableHours.push(startTime + ' - ' + endTime);
-        }
-
-        const availableHours = auxAvailableHours.filter(hour => !auxBusyHours.includes(hour));
-        setAvailableHours(availableHours);
       }
-    } catch (error) {
-      console.log("Error al obtener los horarios", error);
+      auxAvailableHours.push(startTime + ' - ' + endTime);
     }
+
+    const availableHours = auxAvailableHours.filter(hour => !busyHours.includes(hour));
+    return availableHours;
   };
 
   const onSubmit = async (values) => {
-
-    console.log("values: ", values)
+    values.usuario_id = user.id;
+    const [hora_inicio, hora_fin] = values.hour.split(' - '); 
+    values.hora_inicio = hora_inicio;
+    values.hora_fin = hora_fin;
 
     try {
       const response = await reserveRequest(values);
+      alert("Reserva realizada con éxito");
       console.log(response);
+
+      if (response.data) {
+        const newEvent = {
+          id: response.data.id,  // Usa el ID de la reserva devuelto por el backend
+          title: `Reserva ${values.recurso_id}`,
+          start: new Date(`${values.fecha}T${values.hora_inicio}`),
+          end: new Date(`${values.fecha}T${values.hora_fin}`),
+          type: selectedResourceType
+        };
+        setEvents(prevEvents => [...prevEvents, newEvent]); // Actualiza los eventos del calendario
+      }
     } catch (error) {
       console.log("Error al enviar la solicitud:", error);
+      alert("Hora ocupada intente otra");
+    }
+  };
+
+  const handleDeleteEvent = async (event) => {
+    if (window.confirm(`¿Estás seguro de que deseas eliminar la reserva: ${event.title}?`)) {
+      try {
+        // Cambia la ruta al backend donde tienes definida la eliminación de reserva
+        const response = await axios.delete('http://localhost:3000/api/eliminarReserva', {
+          data: { idReserva: event.id }
+        });
+        console.log(response.data.message);
+
+        // Eliminar el evento del estado local
+        setEvents(prevEvents => prevEvents.filter(e => e.id !== event.id));
+        alert('Reserva eliminada con éxito');
+      } catch (error) {
+        console.error('Error al eliminar la reserva:', error);
+        alert('Hubo un problema al eliminar la reserva');
+      }
     }
   };
 
   return (
-    <div>
-      {/* Calendario (Componente separado) */}
-      <Calendary />
+    <div className="form-container">
+      <Calendary events={events} onDeleteEvent={handleDeleteEvent} />
 
-      {/* Formulario */}
-      <h1>Formulario</h1>
+      <h1 className="form-title">Formulario de Reserva</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <label>Tipo de recurso</label>
+          <label className="form-label">Tipo de recurso</label>
           <select 
-            {...register("resourceType", { required: "Selecciona un tipo de recurso" })}
+            {...register("recurso_id", { required: "Selecciona un tipo de recurso" })}
+            value={selectedResourceType}
             onChange={GetResourcesByType}
+            className="form-select"
           >
             <option value="">Seleccionar...</option>
-            <option value="CUBICULO">Cubiculo</option>
-            <option value="GIMNASIO">Gimnasio</option>
+            {user.role === 'estudiante' && (
+              <>
+                <option value="CUBICULO">Cubículo</option>
+                <option value="GIMNASIO">Gimnasio</option>
+                <option value="BIBLIOTECA">Biblioteca</option>
+              </>
+            )}
+            {user.role === 'docente' && (
+              <>
+                <option value="SALA">Sala</option>
+                <option value="GIMNASIO">Gimnasio</option>
+                <option value="BIBLIOTECA">Biblioteca</option>
+              </>
+            )}
           </select>
         </div>
 
-        <div style={{ position: 'relative' }}>
-          <label>Seleccionar Día</label>
+        <div>
+          <label className="form-label">Seleccionar Día</label>
           <DatePicker
             selected={startDate}
             onChange={(date) => {
@@ -141,57 +193,31 @@ export default function Formulary() {
             placeholderText="Seleccionar fecha"
             required
             disabled={!isResourceTypeSelected} 
+            className="form-input"
           />
-          {!isResourceTypeSelected && (
-            <div 
-              style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                width: '100%',
-                height: '100%',
-                background: 'transparent',
-                cursor: 'not-allowed'
-              }}
-              onClick={() => handleBlockedClick('date')}
-            />
-          )}
           {errorMessages.date && (
-            <p style={{ color: 'red' }}>{errorMessages.date}</p>
+            <p className="error-message">{errorMessages.date}</p>
           )}
         </div>
 
-        <div style={{ position: 'relative' }}>
-          <label>Seleccione la hora</label>
+        <div>
+          <label className="form-label">Seleccione la hora</label>
           <select 
             {...register("hour", { required: "Selecciona una hora" })}
             disabled={!isResourceTypeSelected} 
+            className="form-select"
           >
             <option value="">Seleccionar...</option>
             {availableHours.map(hour => (
               <option key={hour} value={hour}>{hour}</option>
             ))}
           </select>
-          {!isResourceTypeSelected && (
-            <div 
-              style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                width: '100%',
-                height: '100%',
-                background: 'transparent',
-                cursor: 'not-allowed'
-              }}
-              onClick={() => handleBlockedClick('hour')}
-            />
-          )}
           {errorMessages.hour && (
-            <p style={{ color: 'red' }}>{errorMessages.hour}</p>
+            <p className="error-message">{errorMessages.hour}</p>
           )}
         </div>
 
-        <button type="submit" disabled={!isResourceTypeSelected}>Enviar</button>
+        <button type="submit" className="submit-button" disabled={!isResourceTypeSelected}>Enviar</button>
       </form>
     </div>
   );
